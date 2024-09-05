c161eed1111352368d5491012551a50179ce047f
Chiranjeev2024 <sks7369064258@gmail.com>
2024-09-06 03:48:51 +0530

    added more logics

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("reset-Btn");
+let newGameBtn = document.querySelector("#new-Btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
 
 let turnO = true;
 
@@ -16,7 +20,7 @@ let winPatterns = [
 
 boxes.forEach((box) => {
     box.addEventListener("click",() => {
-        console.log("box and clicked");
+        // console.log("box and clicked");
         if(turnO){
             box.innerText = "O";
             turnO = false;
@@ -27,5 +31,28 @@ boxes.forEach((box) => {
         }
 
         box.disabled = true;
-    })
-})
+
+        checkWinner();
+    });
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulation, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if(pos1val === pos2val && pos2val === pos3val){
+                console.log("Winner", pos1val);
+                showWinner(pos1val);
+            }
+        }
+    }
+};
+
